9830e7da60dc14b1648a14594d711cb09f8f0d40
Stuart <stuartbudd@gmail.com>
2019-01-24 19:29:37 -0800

    Built out app with main screen and gallery screen.

--- app/src/main/java/com/example/stubu_000/myapplication/MainActivity.java
+++ app/src/main/java/com/example/stubu_000/myapplication/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.stubu_000.myapplication;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int SEARCH_ACTIVITY_REQUEST_CODE = 0;
+    static final int CAMERA_REQUEST_CODE = 1;
+    private String currentPhotoPath = null;
+    private int currentPhotoIndex = 0;
+    private ArrayList<String> photoGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void search(View v) {
+        Intent i = new Intent(MainActivity.this, SearchActivity.class);
+        startActivityForResult(i, SEARCH_ACTIVITY_REQUEST_CODE);
     }
 }
