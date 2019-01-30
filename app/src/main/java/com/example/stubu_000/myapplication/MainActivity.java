5828b86e517a35b4cea773076cbe628dd35ef13a
Sergei Dobrianski <sadoberman@gmail.com>
2019-01-29 22:00:45 -0800

    New Window and text field added, as well back button. Test added

--- app/src/main/java/com/example/stubu_000/myapplication/MainActivity.java
+++ app/src/main/java/com/example/stubu_000/myapplication/MainActivity.java
@@ -1,25 +1,136 @@
 package com.example.stubu_000.myapplication;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String MyApp8_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final int SEARCH_ACTIVITY_REQUEST_CODE = 0;
+    static final int CAMERA_REQUEST_CODE = 1;
+    private String currentPhotoPath = null;
+    private int currentPhotoIndex = 0;
+    private ArrayList<String> photoGallery;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private ImageView ImView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnLeft = (Button)findViewById(R.id.btnLeft);
+        Button btnRight = (Button)findViewById(R.id.btnRight);
+        //Not sure what this button will be, search button?
+        //Button btnFilter = (Button)findViewById(R.id.btnFilter);
+        ImView = (ImageView) findViewById(R.id.imageViewer);
+
+        Date minDate = new Date(Long.MIN_VALUE);
+        Date maxDate = new Date(Long.MAX_VALUE);
+        photoGallery = populateGallery(minDate, maxDate);
+        Log.d("onCreate, size", Integer.toString(photoGallery.size()));
+        if (photoGallery.size() > 0)
+            currentPhotoPath = photoGallery.get(currentPhotoIndex);
+        displayPhoto(currentPhotoPath);
+    }
+    private View.onClickListener filterListener = new View.OnClickListener() {}
+
+    public void search(View v) {
+        Intent i = new Intent(MainActivity.this, SearchActivity.class);
+        startActivityForResult(i, SEARCH_ACTIVITY_REQUEST_CODE);
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText1);
-        String message = editText.getText().toString();
-        intent.putExtra(MyApp8_MESSAGE, message);
-        startActivity(intent);
+   /* private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }*/
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = ImView.getWidth();
+        int targetH = ImView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        ImView.setImageBitmap(bitmap);
+    }
+
 }
---
+++ app/src/main/java/com/example/stubu_000/myapplication/SearchActivity.java
@@ -0,0 +1,43 @@
+package com.example.stubu_000.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import java.util.Calendar;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private EditText fromDate;
+    private EditText toDate;
+    private Calendar fromCalendar;
+    private Calendar toCalendar;
+    private DatePickerDialog.OnDateSetListener fromListener;
+    private DatePickerDialog.OnDateSetListener toListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        fromDate = (EditText) findViewById(R.id.search_fromDate);
+        toDate   = (EditText) findViewById(R.id.search_toDate);
+    }
+
+
+    public void cancel(final View v) {
+        Intent startNewActivity = new Intent(this, MainActivity.class);
+        startActivity(startNewActivity);
+        //finish();
+    }
+
+    public void search(final View v) {
+        Intent i = new Intent(SearchActivity.this, MainActivity.class);
+        i.putExtra("STARTDATE", fromDate.getText().toString());
+        i.putExtra("ENDDATE", toDate.getText().toString());
+        setResult(RESULT_OK, i);
+        finish();
+    }
+}
+
